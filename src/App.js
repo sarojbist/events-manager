ce49aca32b129922766f11e541ddf33c029d6483
sarojbist <bistsaroj24@gmail.com>
2024-08-16 20:47:31 +0530

    Add calandar structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  
+  function changeValue(val) {
+    setDate(val);
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div>
+<div>
+  <
+<Calendar onChange = {changeValue}   value = {date} />
+<p>The selected date is - {date.toLocaleDateString()}</p>
+</div>
+<div>
+  {/* I'm creating a form here so I can add events to the selected date */}
+</div>
+   </div>
+   
+   </>
   );
 }
 
