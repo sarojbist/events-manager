d9d386d1b017b3043799dc280b2283234f9ebeab
sarojbist <bistsaroj24@gmail.com>
2024-08-18 15:50:48 +0530

    add filter buttons

--- src/components/showEvents.js
+++ src/components/showEvents.js
@@ -1,19 +1,25 @@
-import React, { useContext, useState } from 'react';
-import eventsContext from '../Context/Events/EventsContext';
-import { FaEdit, FaTrashAlt } from 'react-icons/fa';
-import AddEvent from './addEvent';
+import React, { useContext, useState } from "react";
+import eventsContext from "../Context/Events/EventsContext";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import AddEvent from "./addEvent";
 
 const ShowEvents = () => {
     const { events, setEvents } = useContext(eventsContext);
     const [showAddEvent, setShowAddEvent] = useState(false);
     const [eventToEdit, setEventToEdit] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState('all');
+
+    const categories = ['all', 'personal', 'work', 'family'];
+    const handleCategoryChange = (category) => {
+        setSelectedCategory(category);
+    };
 
     const handelDeleteEvent = (id) => {
         const updatedEvents = events
             .filter((event) => event.id !== id)
             .map((event, index) => ({
                 ...event,
-                id: index + 1
+                id: index + 1,
             }));
         setEvents(updatedEvents);
     };
@@ -22,7 +28,9 @@ const ShowEvents = () => {
         setEventToEdit(event);
         setShowAddEvent(true);
     };
-
+    const filteredEvents = selectedCategory === 'all'
+        ? events
+        : events.filter(event => event.category === selectedCategory);
     return (
         <div className="overflow-x-auto">
             {showAddEvent && (
@@ -32,25 +40,55 @@ const ShowEvents = () => {
                     eventToEdit={eventToEdit}
                 />
             )}
-
+            {events && events.length > 0 ? (
+                <div className='flex justify-start items-center mb-4'>
+                    {categories.map((category) => (
+                        <button
+                            key={category}
+                            className={`bg-pink-500 text-white py-2 px-4 rounded-md hover:bg-pink-600 mx-2 ${selectedCategory === category ? 'bg-pink-700' : ''}`}
+                            onClick={() => handleCategoryChange(category)}
+                        >
+                            {category.charAt(0).toUpperCase() + category.slice(1)}
+                        </button>
+                    ))}
+                </div>
+            ) : ("")}
             <table className="min-w-full bg-white border border-gray-200">
                 <thead>
                     <tr className="bg-gray-200">
-                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">Event</th>
-                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">Category</th>
-                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">Message</th>
-                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">Date</th>
-                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">Options</th>
+                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">
+                            Event
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">
+                            Category
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">
+                            Message
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">
+                            Date
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-medium text-gray-700">
+                            Options
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                     {events && events.length > 0 ? (
-                        events.map((event) => (
+                        filteredEvents.map((event) => (
                             <tr key={event.id} className="hover:bg-gray-100">
-                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">{event.event}</td>
-                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">{event.category}</td>
-                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">{event.message}</td>
-                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">{new Date(event.date).toLocaleString()}</td>
+                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">
+                                    {event.event}
+                                </td>
+                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">
+                                    {event.category}
+                                </td>
+                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">
+                                    {event.message}
+                                </td>
+                                <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">
+                                    {new Date(event.date).toLocaleString()}
+                                </td>
                                 <td className="px-6 py-4 border-b border-gray-300 text-sm text-gray-700">
                                     <div className="flex items-center space-x-4">
                                         <button onClick={() => handleEditClick(event)}>
@@ -65,13 +103,17 @@ const ShowEvents = () => {
                         ))
                     ) : (
                         <tr>
-                            <td colSpan="5" className="px-6 py-4 text-center text-gray-500">No events to display</td>
+                            <td colSpan="5" className="px-6 py-4 text-center text-gray-500">
+                                No events to display
+                            </td>
                         </tr>
                     )}
                 </tbody>
             </table>
         </div>
     );
-}
+};
 
 export default ShowEvents;
+
+//lets add some filters so users can click on them and it will show events of the selected category only
